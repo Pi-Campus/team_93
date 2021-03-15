6030131e4ba514b5aefc759c65f268944c912914
Luca Carducci <14071215+lucarducci@users.noreply.github.com>
2021-03-15 20:22:15 +0100

    example api

--- server/controllers/Pihack_db/UserController.js
+++ server/controllers/Pihack_db/UserController.js
@@ -14,7 +14,6 @@ import Errors from "../../classes/Errors";
 import ErrorManager from "../../classes/ErrorManager";
 
 const customControllers = {
-  
   /**
    * Override here your custom routes
    * EXAMPLE:
@@ -49,20 +48,16 @@ const customControllers = {
     }
 
    */
-   
 
   // Custom APIs
 
   /**
    * Init routes
    */
-  init: router => {
+  init: (router) => {
     const baseUrl = `${Properties.api}/user`;
-    router.post(
-      baseUrl + "/:id/changePassword",
-      authorize(["ADMIN"]),
-      customControllers.changePassword
-    );
+    router.post(baseUrl + "/:id/changePassword", authorize(["ADMIN"]), customControllers.changePassword);
+    router.get(baseUrl + "/myapi", customControllers.myapi);
 
     UserControllerGenerated.init(router);
   },
@@ -73,28 +68,41 @@ const customControllers = {
    *   @returns object
    *
    */
-  changePassword: async (req, res) => {
+  myapi: async (req, res) => {
     try {
-      const user = await UserModel.getByUsernameAndPassword(
-        req.user.username,
-        req.body.passwordAdmin
-      );
-      if (!user) {
-        throw new Errors.PWD_ADMIN_NOT_VALID();
-      }
-      await UserModel.updatePassword(req.params.id, req.body.passwordNew);
       res.send({
-        success: true
+        success: true,
       });
     } catch (err) {
       const safeErr = ErrorManager.getSafeError(err);
       res.status(safeErr.status).json(safeErr);
     }
-  }
+  },
+
+  /**
+   * UserModel.changePassword
+   *   @description Change password of user from admin
+   *   @returns object
+   *
+   */
+  changePassword: async (req, res) => {
+    try {
+      const user = await UserModel.getByUsernameAndPassword(req.user.username, req.body.passwordAdmin);
+      if (!user) {
+        throw new Errors.PWD_ADMIN_NOT_VALID();
+      }
+      await UserModel.updatePassword(req.params.id, req.body.passwordNew);
+      res.send({
+        success: true,
+      });
+    } catch (err) {
+      const safeErr = ErrorManager.getSafeError(err);
+      res.status(safeErr.status).json(safeErr);
+    }
+  },
 };
 
 export default {
   ...UserControllerGenerated,
-  ...customControllers
+  ...customControllers,
 };
-
